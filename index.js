c012acd5a7d64f1fc731c96392c9308a236b1fd1
Anas Abubakar <anasabubakar2015@gmail.com>
2022-07-04 15:46:14 +0100

    Add the awsome book javascript code using modules

---
+++ index.js
@@ -0,0 +1,16 @@
+import * as books from './modules/books.js';
+import * as navigation from './modules/navigation.js';
+import dateTime from './modules/date.js'
+
+dateTime();
+
+navigation.allLinks.addEventListener('click', (e) => {
+  const clicked = e.target;
+  if (!clicked.classList.contains('link')) return;
+  navigation.allSection.forEach((section) => {
+    section.classList.remove('active');
+  });
+  document
+    .querySelector(`.section-${clicked.dataset.link}`)
+    .classList.add('active');
+});
